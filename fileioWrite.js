589bfc9c75fedc3bf14119884c4703c156de786b
phapdinh <centralparkloop@yahoo.com>
2016-06-27 00:04:04 -0400

    changed fileioWrite to take in multiple inputs to write

--- fileioWrite.js
+++ fileioWrite.js
@@ -3,12 +3,16 @@ var filesys = require("fs");
 //get name of file to open
 var fileName = process.argv.slice(2)[0];
 //get input to write to file
-var fileInput = process.argv.slice(2)[1];
+var fileInput = process.argv.slice(2);
+var output = '';
+for(var i = 1; i < fileInput.length; i++) {
+	output += fileInput[i] + ' \n';
+}
 
 //Writes a file called output.txt
-filesys.writeFile(fileName, fileInput, (err) => {
+filesys.writeFile(fileName, output, (err) => {
 	if(err) throw err;
-	console.log('Wrote output.txt');
+	console.log('Wrote output file');
 });
 
 //this will display first because callback function is asynchronous
